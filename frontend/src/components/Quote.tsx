cbf1a9c122e119564a883089400f977ba27e669c
tejas <pangaonkar.svc424@gmail.com>
2024-12-05 16:37:25 +0530

    added som

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,7 +1,9 @@
+import quoteImage from '../assets/Quote.jpg'
+
 export const Quote = () => {
     return <div>
         <div className="p-20 pb-0 ">
-            <img className="rounded-md shadow-xl" src="/src/assets/Quote.jpg" alt="Blog Image" />
+            <img className="rounded-md shadow-xl" src={quoteImage} alt="Blog Image" />
         </div>
         <div className="flex justify-end">
             .
